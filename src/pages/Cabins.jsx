8f4370790441888412604c1d0d988a12666e9613
apit putra <apiiettrecomen1@gmail.com>
2024-02-21 07:26:36 +0700

    refactor: change form to modal form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,24 +1,20 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable.jsx";
-import Button from "../ui/Button.jsx";
-import {useState} from "react";
-import CreateCabinForm from "../features/cabins/CreateCabinForm.jsx";
+import AddCabin from "../features/cabins/AddCabin.jsx";
 
 function Cabins() {
-    const [showForm, setShowForm] = useState(false);
-    return (
-        <>
-            <Row type="horizontal">
-                <Heading as="h1">All cabins</Heading>
-            </Row>
-            <Row>
-                <CabinTable/>
-                <Button onClick={() => setShowForm(show => !show)}>Add new cabin</Button>
-                {showForm && <CreateCabinForm/>}
-            </Row>
-        </>
-    );
+  return (
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+      </Row>
+      <Row>
+        <CabinTable />
+        <AddCabin />
+      </Row>
+    </>
+  );
 }
 
 export default Cabins;
